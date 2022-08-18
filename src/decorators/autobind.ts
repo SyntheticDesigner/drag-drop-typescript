636f0618651ffb97f00a3174cbc4da2c956ee04b
SyntheticDesigner <andrew.sch.j@gmail.com>
2022-08-17 17:18:34 -0700

    split code into multiple files using namespace

---
+++ src/decorators/autobind.ts
@@ -0,0 +1,18 @@
+namespace App {
+  //autobind decorator
+  export function autobind(
+    _: any, //target
+    _2: string, //methodName
+    descriptor: PropertyDescriptor
+  ) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+      configurable: true,
+      get() {
+        const boundFn = originalMethod.bind(this);
+        return boundFn;
+      },
+    };
+    return adjDescriptor;
+  }
+}
